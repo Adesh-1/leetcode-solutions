e8c5374105010619512aafed875eb0b05c91e454
Adesh-1 <adesh717262@gmail.com>
2025-07-22 20:37:17 +0530

    fix: correctly handle in-place duplicate removal in sorted array

---
+++ easy/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+/* 26. Remove Duplicates from Sorted Array */
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int curr = 0;
+       for(int i = 1; i < nums.length; i++){
+        if(nums[curr] != nums[i]){
+            curr++;
+            nums[curr] = nums[i];  
+        } 
+       }
+       return curr + 1;
+    }
+}
