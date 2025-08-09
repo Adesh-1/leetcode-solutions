d10d31ab2a1b374802d8e31afa2a7e0ed563ef92
Adesh 👋👋 <adesh717262@gmail.com>
2025-08-09 21:17:28 +0530

    leetcode ques 69

--- easy/Ques69.java
+++ easy/Ques69.java
@@ -5,3 +5,11 @@ import math as m
 class Solution:
     def mySqrt(self, x: int) -> int:
         return int(m.sqrt(x))
+
+// In Java
+import static java.lang.Math.sqrt;
+class Solution {
+    public int mySqrt(int x) {
+        return (int)sqrt(x);
+    }
+}
