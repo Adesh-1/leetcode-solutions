c9655e4f90deccdecbd2a88fabfe54ae385cde1a
Adesh 👋👋 <adesh717262@gmail.com>
2025-08-12 22:50:39 +0530

    leetcode ques 1672
    fix a bug

--- easy/Ques1672.java
+++ easy/Ques1672.java
@@ -11,7 +11,7 @@ class Solution:
   // in java
   class Solution {
     public int maximumWealth(int[][] accounts) {
-        int richest = 0;
+        int richest = Integer.MIN_VALUE;
         for(int i = 0; i < accounts.length; i++){
             int sum = 0;
             for(int j = 0; j < accounts[i].length; j++){
