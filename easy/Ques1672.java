e46f54def4a352dd1949355870e11271156498dc
Adesh 👋👋 <adesh717262@gmail.com>
2025-08-12 21:08:42 +0530

    leetcode ques 1672

---
+++ easy/Ques1672.java
@@ -0,0 +1,25 @@
+// 1672. Richest Customer Wealth
+// in python
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        richest = 0
+        for i in range(len(accounts)):
+            if richest < sum(accounts[i]):
+                richest = sum(accounts[i])
+        return richest
+
+  // in java
+  class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int richest = 0;
+        for(int i = 0; i < accounts.length; i++){
+            int sum = 0;
+            for(int j = 0; j < accounts[i].length; j++){
+               sum += accounts[i][j];
+            }
+            if(richest < sum)
+                richest = sum;
+        }
+        return richest;
+    }
+}
