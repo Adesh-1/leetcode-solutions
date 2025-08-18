d95f266cf4e38bef0c53fc2d06842e0ab9de411e
Adesh 👋👋 <adesh717262@gmail.com>
2025-08-18 22:32:19 +0530

    leetcode ques 50

---
+++ medium/Ques50.py
@@ -0,0 +1,12 @@
+# 50. Pow(x, n)
+# in py
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return math.pow(x,n)
+
+# in java
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
