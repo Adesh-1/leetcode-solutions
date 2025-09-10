d39f69ebdc4e404774fbf44fae17bd2cff62fafb
Adesh 👋👋 <adesh717262@gmail.com>
2025-09-10 20:59:26 +0530

    Binary Search

---
+++ easy/Ques704.py
@@ -0,0 +1,18 @@
+# 704. Binary Search
+# in python
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        return -1
+
+# in java
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
+}
