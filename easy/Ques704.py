5c798e8cc5e088ebbb008f0d68325a5fae702b52
Adesh 👋👋 <adesh717262@gmail.com>
2025-09-10 21:11:49 +0530

    Binary Search

--- easy/Ques704.py
+++ easy/Ques704.py
@@ -7,6 +7,25 @@ class Solution:
         return -1
 
 # in java
+# binary search
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int val = nums[mid];
+            if (val == target)
+                return mid;
+            else if (val < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
+
+# linear search
 class Solution {
     public int search(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
