618ed25f4408492c28da1740f7efc64d68b31567
Adesh 👋👋 <adesh717262@gmail.com>
2025-09-16 22:46:00 +0530

    Valid Parentheses

--- easy/Ques20.java
+++ easy/Ques20.java
@@ -1,4 +1,5 @@
 // 20. Valid Parentheses
+// in pyhton
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
@@ -18,3 +19,31 @@ class Solution:
                         return False
 
         return not stack
+
+    // in java
+    class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // Push opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack[++top] = ch;
+            } 
+            // Check closing brackets
+            else if (top == -1 || 
+                     (ch == ')' && stack[top] != '(') || 
+                     (ch == '}' && stack[top] != '{') || 
+                     (ch == ']' && stack[top] != '[')) {
+                return false;
+            } else {
+                top--; // pop if matched
+            }
+        }
+
+        return top == -1;
+    }
+}
