65db1455198558ed0169e2594fe48542fdbc0264
Adesh 👋👋 <adesh717262@gmail.com>
2025-09-30 22:35:26 +0530

    Add Two Integers

---
+++ easy/Ques2235.java
@@ -0,0 +1,12 @@
+// 2235. Add Two Integers
+// in java
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+// in python
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
