ae850ceaa12ee0064c527136761b579aa5a66239
Adesh 👋👋 <adesh717262@gmail.com>
2025-10-03 21:44:31 +0530

    Add Strings

---
+++ easy/Ques415.java
@@ -0,0 +1,12 @@
+// 415. Add Strings
+// in java
+import java.math.BigInteger;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        BigInteger res = n1.add(n2);
+        return res.toString();
+    }
+}
