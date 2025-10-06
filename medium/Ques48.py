c392c9b64a5448af2421ba8eb9d72bb8d1085e1d
Adesh 👋👋 <adesh717262@gmail.com>
2025-10-06 23:02:41 +0530

    Rotate Image

---
+++ medium/Ques48.py
@@ -0,0 +1,46 @@
+# 48. Rotate Image
+# in python
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Step 1: Transpose
+        for i in range(n):
+            for j in range(i + 1, n):  # here i+1 bcz diagonal remains same
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Step 2: Reverse each row
+        for row in matrix:
+            row.reverse()
+
+
+# in java
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        # // Step 1: Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        # // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
