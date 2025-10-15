cffb08d6f1a05781ab6327fe044d231876e3a4dd
Adesh 👋👋 <adesh717262@gmail.com>
2025-10-15 22:11:59 +0530

    Factorial Trailing Zeroes

---
+++ medium/Ques172.java
@@ -0,0 +1,21 @@
+// 172. Factorial Trailing Zeroes
+// in java
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n >= 5) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
+
+// in python
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n >= 5:
+            n //= 5
+            count += n
+        return count
