90b07ec46d82a37fa0c3b9c953a812b196225939
Adesh 👋👋 <adesh717262@gmail.com>
2025-10-28 21:57:23 +0530

    Divisor Game
    Implement the Divisor Game logic in Java.

---
+++ easy/Ques1025.java
@@ -0,0 +1,21 @@
+// 1025. Divisor Game
+
+      /*
+            alice wins if n is even and loses if n is odd.
+            if n %2 == 0:
+                return True 
+            else: 
+                return False
+        */
+
+// in java
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
+
+// in python
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
