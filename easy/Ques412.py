fd6e02cb1adcba6806ed0ab2a8331c5747506d53
Adesh 👋👋 <adesh717262@gmail.com>
2025-11-06 21:27:53 +0530

    Fizz Buzz

---
+++ easy/Ques412.py
@@ -0,0 +1,34 @@
+# 412. Fizz Buzz
+# in python
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                answer.append("FizzBuzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(f"{i}")
+        return answer
+
+# in java
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
