dbd319f43a70352fb2798e107d8cf35027c7c841
Adesh 👋👋 <adesh717262@gmail.com>
2025-11-24 22:45:43 +0530

    Binary Prefix Divisible By 5

---
+++ easy/Ques1018.py
@@ -0,0 +1,23 @@
+# 1018. Binary Prefix Divisible By 5
+# in pthon
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        res = []
+        cur = 0  # current prefix value modulo 5
+        for bit in nums:
+            cur = ((cur << 1) + bit) % 5   # same as cur = (cur * 2 + bit) % 5
+            res.append(cur == 0)
+        return res
+
+# in java
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+        int cur = 0;
+        for (int bit : nums) {
+            cur = ((cur << 1) + bit) % 5;
+            res.add(cur == 0);
+        }
+        return res;
+    }
+}
