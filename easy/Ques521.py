29271182bb0b9406925d1d356dda719cd454272f
Adesh 👋👋 <adesh717262@gmail.com>
2025-12-02 22:55:56 +0530

    Longest Uncommon Subsequence I

---
+++ easy/Ques521.py
@@ -0,0 +1,16 @@
+# 521. Longest Uncommon Subsequence I
+# in python
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a), len(b))
+
+# in java
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
