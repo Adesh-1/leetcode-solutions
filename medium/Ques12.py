9e0dae5f37a9184b43da18e5cf408ae4efe09c80
Adesh 👋👋 <adesh717262@gmail.com>
2025-12-19 23:04:47 +0530

    Integer to Roman

---
+++ medium/Ques12.py
@@ -0,0 +1,44 @@
+# 12. Integer to Roman
+# in python
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+
+        res = []
+        for val, sym in values:
+            while num >= val:
+                res.append(sym)
+                num -= val
+
+        return "".join(res)
+
+# in java
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return sb.toString();
+    }
+}
