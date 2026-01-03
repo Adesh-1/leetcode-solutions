fcd5e32c0b5083b74f4d21d273f262212ea04902
Adesh 👋👋 <adesh717262@gmail.com>
2026-01-03 21:14:16 +0530

    Array Partition

---
+++ easy/Ques561.py
@@ -0,0 +1,20 @@
+# 561. Array Partition
+# in python
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        sum = 0
+        for i in range(0, len(nums), 2):
+            sum += nums[i]
+        return sum
+
+# in java
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2)
+            sum += nums[i];
+        return sum;
+    }
+}
