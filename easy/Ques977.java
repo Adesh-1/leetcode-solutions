f10294f243f15f87a4a6018a8e02530b9ead5bd4
Adesh 👋👋 <adesh717262@gmail.com>
2026-01-17 23:36:24 +0530

    Squares of a Sorted Array

--- easy/Ques977.java
+++ easy/Ques977.java
@@ -11,8 +11,25 @@ class Solution {
 }
 
 // in pyhton
+    // 1st method 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         sq_list = [i**2 for i in nums]
         sq_list.sort()
         return sq_list
+
+    // 2nd method  O(n) time complexity
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        sq = [0] * n
+        l, r = 0, n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[l]) > abs(nums[r]):
+                val = nums[l]
+                l += 1
+            else:
+                val = nums[r]
+                r -= 1
+            sq[i] = val**2
+        return sq
