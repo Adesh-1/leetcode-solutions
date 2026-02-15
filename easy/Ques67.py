93770bfc59a39d69997be1debe6597a409e48c05
Adesh 👋👋 <adesh717262@gmail.com>
2026-02-15 23:29:51 +0530

    Add Binary

--- easy/Ques67.py
+++ easy/Ques67.py
@@ -1,9 +1,15 @@
 # 67. Add Binary
 # in python
+    # 1st method
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         total = int(a, 2) + int(b, 2)
         return bin(total)[2:]
+    
+    # 2nd method
+class Solution:
+    def addBinary(self, a: str, b: str) -> str: 
+        return f"{int(a, 2) + int(b, 2):b}"
 
 # in java
 import java.math.BigInteger;
