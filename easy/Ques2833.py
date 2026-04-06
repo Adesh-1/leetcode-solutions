f0c02cf13ea72da51e28a2e43b294833d3a3908d
Adesh 👋👋 <adesh717262@gmail.com>
2026-04-06 23:29:36 +0530

    Furthest Point From Origin

---
+++ easy/Ques2833.py
@@ -0,0 +1,22 @@
+# 2833. Furthest Point From Origin
+# in python
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        m, n = moves.count('L'), moves.count('R')
+        return moves.count('_') + (max(m, n) - min(m, n))
+
+# in java
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int l = 0, r = 0, x = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'L')
+                l++;
+            else if (ch == 'R')
+                r++;
+            else
+                x++;
+        }
+        return x + (Math.max(l, r) - Math.min(l, r));
+    }
+}
