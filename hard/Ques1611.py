207c9950952c0532a45a44ba722329c0a9b763d1
Adesh 👋👋 <adesh717262@gmail.com>
2026-04-09 22:57:33 +0530

    Minimum One Bit Operations to Make Integers Zero

---
+++ hard/Ques1611.py
@@ -0,0 +1,21 @@
+# 1611. Minimum One Bit Operations to Make Integers Zero
+# in python
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        res = 0
+        while n: # n>0
+            res ^= n
+            n >>= 1
+        return res
+
+# in java
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+}
