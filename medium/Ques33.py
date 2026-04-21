82a65837c68fb62c1b82ea61d2f89fe991f5c3fa
Adesh 👋👋 <adesh717262@gmail.com>
2026-04-21 11:32:58 +0530

    Search in Rotated Sorted Array

--- medium/Ques33.py
+++ medium/Ques33.py
@@ -1,17 +1,59 @@
 # 33. Search in Rotated Sorted Array
 # in python
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
+    def search(self, nums, target):
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Left half is sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            # Right half is sorted
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
         return -1
 
   # in java
-  class Solution {
+ class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target)
-                return i;
+        int si = 0;
+        int ei = nums.length - 1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            # // search on L1 
+            if (nums[si] <= nums[mid]) {
+                # // in left
+                if (nums[si] <= target && target < nums[mid])
+                    ei = mid - 1;
+                # // in right
+                else
+                    si = mid + 1;
+            }
+            # // search on L2 
+            else {
+                # // in right
+                if (nums[mid] < target && target <= nums[ei])
+                    si = mid + 1;
+                # // in left
+                else
+                    ei = mid - 1;
+            }
         }
         return -1;
     }
