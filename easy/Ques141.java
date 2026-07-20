5987d1bd9c3e36fb06a9d48a193506622483c4d4
Adesh 👋👋 <adesh717262@gmail.com>
2026-07-20 19:28:17 +0530

    Linked List Cycle

---
+++ easy/Ques141.java
@@ -0,0 +1,25 @@
+// 141. Linked List Cycle
+// in python
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+// in java
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
