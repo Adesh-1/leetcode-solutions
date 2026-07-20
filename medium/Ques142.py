49121d6a3fd7940a71a148daf355d62c40eb5417
Adesh 👋👋 <adesh717262@gmail.com>
2026-07-20 19:53:58 +0530

    Linked List Cycle II

---
+++ medium/Ques142.py
@@ -0,0 +1,36 @@
+# 142. Linked List Cycle II
+# in python
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return fast
+        return None
+      
+# in java
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return fast;  # // or can return slow
+            }
+        }
+        return null;
+    }
+}
