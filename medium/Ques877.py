ae25bd748a45d37c87cdf50981e7f288746edfba
Adesh 👋👋 <adesh717262@gmail.com>
2026-08-02 14:24:36 +0530

    Stone Game

---
+++ medium/Ques877.py
@@ -0,0 +1,12 @@
+# 877. Stone Game
+# in python
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+
+# in java
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
