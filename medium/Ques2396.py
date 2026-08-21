64ba5e3042502b671c6d9ab9ba2f5d1301938957
Adesh 👋👋 <adesh717262@gmail.com>
2026-08-21 22:04:40 +0530

    Strictly Palindromic Number

---
+++ medium/Ques2396.py
@@ -0,0 +1,12 @@
+# 2396. Strictly Palindromic Number
+# in python
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+# in java
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
