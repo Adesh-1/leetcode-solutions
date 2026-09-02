8bcbd3320cac25f0d639b2c47bc493fe5ee640e3
Adesh 👋👋 <adesh717262@gmail.com>
2026-09-02 20:09:31 +0530

    Construct Uniform Parity Array I
    Added a Python implementation of the uniformArray method.

---
+++ easy/Ques3875.py
@@ -0,0 +1,12 @@
+# 3875. Construct Uniform Parity Array I
+# in python
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+# in java
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
